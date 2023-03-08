60f7d41cf293d38451c9ab1fb1536e0f72949adb
dilshan488 <dilshanpubudu461@gmail.com>
2023-03-08 14:29:24 +0530

    first commit

---
+++ forgit/index.js
@@ -0,0 +1,15 @@
+const app = require('express')();
+const PORT = 4001;
+
+app.listen(
+    PORT,() =>
+
+    console.log('Server is on ${PORT}')
+
+);
+
+app.get('/getRequest',(req,res)=> {
+res.send("this is successful")
+
+}
+);
